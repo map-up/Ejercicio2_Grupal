005678d4e60a025b93c4b173962e1ef35ff9262f
juanpedro2021 <juanpedro103.jp@gmail.com>
2023-08-08 17:27:55 -0300

    Create claseJuan.java
    creo una clase con un metodo

---
+++ src/ejercicio2_grupal/claseJuan.java
@@ -0,0 +1,16 @@
+
+package ejercicio2_grupal;
+
+/**
+ *
+ * @author juanPedro
+ */
+public class claseJuan {
+    private int altura;
+    private int peso;
+    
+    public void mostrar(){
+        System.out.println("mi modificacion");
+    }
+    
+}
